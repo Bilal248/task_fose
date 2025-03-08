52fe11ec00f69398f48f7365d1a9aa30ed4a663d
Bilal Ahmad Khan <bh2448.hb@gmail.com>
2025-03-08 15:03:05 +0500

    Conflict resolved Commited Bilal Ahmad Khan 8 March, 2025 2:29 PM 2:38 PM

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 print("This is Versioning Practically")
 
-<<<<<<< HEAD
 print("let's add only post ;)")
-=======
 print("Now I can proudly upload to my linkedin this new skill git")
->>>>>>> feature/feedback
